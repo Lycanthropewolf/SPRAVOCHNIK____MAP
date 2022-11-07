d03eef84e5080eac7ed444796e860f75fc7ace80
Антон <antonolegovich2017@icloud.com>
2022-11-07 17:01:53 +0600

    DZ

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Spravochnik.addDirectory(" Oleg","86764837");
+        Spravochnik.addDirectory(" DIMA","5689247593485");
+        Spravochnik.addDirectory(" PETYA","3409898723");
+        Spravochnik.addDirectory(" VITALYA","46792");
+        Spravochnik.addDirectory(" NINA","234598457");
+        Spravochnik.addDirectory(" OLGA","562085");
+        Spravochnik.addDirectory(" GENYA","1597475");
+        Spravochnik.addDirectory(" VALENTIN","5688234");
+        Spravochnik.addDirectory(" BORYA","56259684");
+        Spravochnik.addDirectory(" SEREGA","8126876");
+        Spravochnik.addDirectory(" IGOR","463187624");
+        Spravochnik.addDirectory(" VITYA","89531789");
+        Spravochnik.addDirectory(" GLEB","8785652378");
+        Spravochnik.addDirectory(" DENIS","45298532");
+        Spravochnik.addDirectory(" IMAM","45345967");
+        Spravochnik.addDirectory("GERA","345345986");
+        Spravochnik.addDirectory(" YANA","345345943");
+        Spravochnik.addDirectory(" RITA","34534543");
+        Spravochnik.addDirectory(" KATYA","86764837");
+        Spravochnik.addDirectory(" SONYA","86764837");
+        Spravochnik.printDirectoty();
+
+    }
+}
---
+++ src/Spravochnik.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Spravochnik {
+    private static  String name;
+    private static String telefonNamber;
+
+    public static Map<String,String> directory=new HashMap<>();
+
+    public static void addDirectory(String name,String telefonNamber){
+        directory.put(name,telefonNamber);
+    }
+    public static void printDirectoty(){
+        System.out.println(directory.toString());
+    }
+
+}
